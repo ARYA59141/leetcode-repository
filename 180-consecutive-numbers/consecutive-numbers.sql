21eb284330f992a179e19ff2ceece2b7b7deab2b
ARYA59141 <2303A54028@sru.edu.in>
2026-08-11 23:04:23 +0530

    Time: 874 ms (7.81%) | Memory: 0B (100.00%) - LeetSync

---
+++ 180-consecutive-numbers/consecutive-numbers.sql
@@ -0,0 +1,9 @@
+WITH CTE AS (
+    SELECT num,
+           LEAD(num, 1) OVER () AS next_1,
+           LEAD(num, 2) OVER () AS next_2
+    FROM Logs
+)
+SELECT DISTINCT num AS ConsecutiveNums
+FROM CTE
+WHERE num = next_1 AND num = next_2;
