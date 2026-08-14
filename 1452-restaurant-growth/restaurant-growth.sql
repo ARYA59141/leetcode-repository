b45a54dbeab9e38a203478c78ea8661b714eae9e
ARYA59141 <2303A54028@sru.edu.in>
2026-08-14 22:21:56 +0530

    Time: 434 ms (21.73%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1452-restaurant-growth/restaurant-growth.sql
@@ -0,0 +1,24 @@
+WITH DailySum AS (
+    SELECT 
+        visited_on, 
+        SUM(amount) AS amount
+    FROM 
+        Customer
+    GROUP BY 
+        visited_on
+)
+SELECT 
+    a.visited_on,
+    SUM(b.amount) AS amount,
+    ROUND(AVG(b.amount), 2) AS average_amount
+FROM 
+    DailySum a
+JOIN 
+    DailySum b 
+    ON DATEDIFF(a.visited_on, b.visited_on) BETWEEN 0 AND 6
+GROUP BY 
+    a.visited_on
+HAVING 
+    COUNT(b.visited_on) = 7
+ORDER BY 
+    a.visited_on ASC;
